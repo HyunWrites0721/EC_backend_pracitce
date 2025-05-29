aaf05482888709c3b0c7f245391869b4316640a8
Hyun Kang <107344451+HyunWrites0721@users.noreply.github.com>
2025-05-29 17:31:58 +0900

    Update Article.java

--- src/main/java/com/backend/ec/Article.java
+++ src/main/java/com/backend/ec/Article.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 @Entity
