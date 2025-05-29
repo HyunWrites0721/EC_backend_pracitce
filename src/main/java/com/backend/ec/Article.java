836ef7ba4fc8d1b36feaa82f7b86c21f6b319943
Hyun Kang <107344451+HyunWrites0721@users.noreply.github.com>
2025-05-29 17:26:41 +0900

    Update Article.java

--- src/main/java/com/backend/ec/Article.java
+++ src/main/java/com/backend/ec/Article.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String title;
     private String content;
 
