c884fd6e4d835d3d951f2ef22b8702f753418e3a
Hyun Kang <107344451+HyunWrites0721@users.noreply.github.com>
2025-05-29 17:15:44 +0900

    Create Request.java

---
+++ src/main/java/com/backend/ec/Request.java
@@ -0,0 +1,15 @@
+package com.backend.ec;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Request {
+    private String title;
+    private String content;
+}
